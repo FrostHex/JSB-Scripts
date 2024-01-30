1735d6e95a0dc8abbba10e5d100165d7f8c34b4f
FrostHex <frosts_mage@163.com>
2024-01-30 23:21:08 +0800

    🎨style: 整理格式 添加注释

--- CrisisContract.py
+++ CrisisContract.py
@@ -32,23 +32,23 @@ class Contract:
         
         self.score = 0  # 总分数
         self.term_num = len(self.contract_list)  # 合约总数
-        for i in range(self.term_num):  # 初始化合约选中状态
-            self.contract_list[i].append('-')
+        for i in range(self.term_num):  # 生成第三栏'选中状态', 初始化为不勾选
+            self.contract_list[i].append('-')  
 
-    # 展示合约列表
+    # 展示合约信息
     def show_info(self):
+        # 列出合约条目
         for i in range(self.term_num):
             print(f"{self.contract_list[i][2]} {i+1}.{self.contract_list[i][1]} ({self.contract_list[i][0]}分)")
 
-        #计算当前分数
+        # 计算当前分数
         self.score = 0
         for i in range(self.term_num):
             if self.contract_list[i][2] == "√":
                 self.score += self.contract_list[i][0]
         print("-"*15 + f"\n总分数: {self.score}")
 
-
-    # 选择合约条目 (返回值: 0循环 1结束 2报错)
+    # 输入选择合约条目 (返回值: 0循环 1结束 2报错)
     def choose(self):
         ids = input("-"*15 + "\n请选择合约请输入数字序号并回车 (输入多个合约用空格分隔)\n开始运行请直接输入回车\n")
         try:
@@ -56,7 +56,7 @@ class Contract:
                 return 1
             id_list = [int(i) for i in ids.split()]  # 将输入的数字字符串分割成列表, 并转换为int型
             for id in id_list:  # 选中此条合约, 若此条目已选中则将其取消
-                if id <= self.term_num-1 and id > -1:
+                if id <= self.term_num and id > 0:
                     if self.contract_list[id-1][2] == '-':
                         self.contract_list[id-1][2] = '√'
                     else:
@@ -68,13 +68,10 @@ class Contract:
         except:
             print("输入有误!")
             time.sleep(1)
-            return 2
+            return 2  # 目前没用上
 
     # 开始运行合约
     def run(self):
-        print("开始运行合约, 按R键终止程序")
-        self.quit = 0
-
         # 用文档记录分数
         def append_to_score_file(text):
             file_path = "Score.txt"
@@ -88,10 +85,7 @@ class Contract:
             with open(file_path, 'a') as file:
                 file.write(text + "\n")
         
-        append_to_score_file(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\t' + str(self.score))
-        keys = ['w', 'a', 's', 'd', 'up', 'left', 'down', 'right', 'space']
-        arrowkeys = ['w', 'a', 's', 'd', 'up', 'left', 'down', 'right']
-
+        # 生成小悬浮窗提示合约启动
         def start_root(window):
             window.attributes('-topmost', True)  # 置顶窗口
             window.overrideredirect(True)  #隐藏窗口
@@ -108,39 +102,43 @@ class Contract:
         def hide_root(window, delay):
             window.after(delay, lambda: window.geometry("1x1+0+0"))
 
+        # 打印按下的按键
+        # def DetectionThread():
+        #     def KeyboardDetection():
+        #         global pressedkeys 
+        #         pressedkeys = [key for key in keys if keyboard.is_pressed(key)] # 当前按下的按键列表
+        #         print(pressedkeys)
+        #        
+        #     while True:
+        #         KeyboardDetection()
+
+        # keys = ['w', 'a', 's', 'd', 'up', 'left', 'down', 'right', 'space']
+        arrowkeys = ['w', 'a', 's', 'd', 'up', 'left', 'down', 'right']
+        print("开始运行合约, 按R键终止程序")
+        append_to_score_file(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\t' + str(self.score))
         root = tk.Tk()
         start_root(root)
         root.after(2000, lambda: hide_root(root, 0))
-        
+        # detectthread = threading.Thread(target=DetectionThread)
+        # detectthread.start()
 
-        def DetectionThread():
-            def KeyboardDetection():
-                # 当前按下的按键列表
-                global pressedkeys
-                pressedkeys = [key for key in keys if keyboard.is_pressed(key)]
-                
-            while True:
-                KeyboardDetection()
-        detectthread = threading.Thread(target=DetectionThread)
-        #detectthread.start()
 
+        #=================================== 合约内容代码 ===================================#
 
         # 1.冲刺附带两秒冷却
         if self.contract_list[0][2] == "√":
-            spaceblocking = False
             def space_event(e):
-                
                 nonlocal spaceblocking
                 if e.name == "space" and not spaceblocking:
-                    
                     spaceblocking = True
                     keyboard.block_key("space")
                     time.sleep(2)
-                    
                     keyboard.unblock_key("space")
                     keyboard.release("space")
                     spaceblocking = False
-            thread = threading.Thread(target=keyboard.on_press, args=(space_event,))
+            
+            spaceblocking = False
+            thread = threading.Thread(target=keyboard.on_press, args=(space_event,))  # ags传参,附带逗号表示单个元素的元组
             thread.start()
 
         # 2.禁止向左移动
@@ -176,7 +174,7 @@ class Contract:
                     if fkey != key.name and key.name in arrowkeys:
                         keyboard.release(fkey)
             keyboard.on_press(chopsticks)
-            
+
         # 6.遮住屏幕右侧3/4的区域
         if self.contract_list[5][2] == "√":
             def set_topmost(window):
@@ -188,26 +186,23 @@ class Contract:
             def set_geometry(window):
                 screen_width = window.winfo_screenwidth()
                 screen_height = window.winfo_screenheight()
-
                 width = screen_width // 4 * 3  # 右侧四分之三的宽度
                 height = screen_height
-
                 window.geometry(f"{width}x{height}+{screen_width - width}+0")  # 设置窗口位置和大小
 
             mask = tk.Toplevel(root)
             mask.title("右侧四分之三")
             set_topmost(mask)
-            set_geometry(mask)
-            
+            set_geometry(mask)   
+
         # 7.上下翻转屏幕
         if self.contract_list[6][2] == "√":
-
             screen = rotatescreen.get_primary_display()
             screen.rotate_to(180)
 
         # 8.屏幕逐渐变暗，冲刺时恢复
         if self.contract_list[7][2] == "√":
-
+            # 窗口穿透, 防止置顶窗口占用焦点
             def setWinThrowON():
                 while True:
                     hwnd = ctypes.windll.user32.FindWindowW(None, "tempestissimo")
@@ -226,8 +221,8 @@ class Contract:
 
             def set_window(window):
                 window.attributes('-topmost', True)  # 置顶窗口
-                window.overrideredirect(True)  #隐藏窗口
-                window.attributes('-disabled', True)  #禁止点击
+                window.overrideredirect(True)  # 隐藏窗口
+                window.attributes('-disabled', True)  # 禁止点击
                 window.title("tempestissimo") 
                 screen_width = window.winfo_screenwidth()
                 screen_height = window.winfo_screenheight()
@@ -252,7 +247,7 @@ class Contract:
                     for i in range(1,101):
                         set_transparency(storm, int(0.1 * 1.05 ** (i + 42)) / 100)
                         time.sleep(self.linger/100)
-                    # time.sleep(0.5*(2.5-self.linger))  # 额外冷却时间
+                    # time.sleep(0.5*(2.5-self.linger))  # 打闪额外冷却时间
             
             def stromstart():
                 while True:
@@ -265,34 +260,36 @@ class Contract:
 
             self.cooldown = True  # 控制闪电冷却
             storm = tk.Toplevel(root)
-            set_background_color(storm, "black")
+            set_background_color(storm, "black")  # 初始化窗口为半透明
             set_transparency(storm, 0.5)
             set_window(storm)
             original_position = (storm.winfo_x(), storm.winfo_y())
-            stormthread = threading.Thread(target=stromstart)
+            stormthread = threading.Thread(target=stromstart)  # 设置闪屏线程
             stormthread.start()
-            setWinThrowtread = threading.Thread(target=setWinThrowON)
+            setWinThrowtread = threading.Thread(target=setWinThrowON)  # 设置窗口穿透线程
             setWinThrowtread.start()
 
-        # 9.不断窗口出现遮挡屏幕
+        # 9.不断出现窗口遮挡屏幕
         if self.contract_list[8][2] == "√":
             def set_window2(window):
                 window.title("HELLO")
                 window.configure(bg="#fe1f6f")
                 window.attributes('-topmost', True)  # 置顶窗口
-                window.attributes('-disabled', True)  #禁止点击
+                window.attributes('-disabled', True)  # 禁止点击
                 sidelength = window.winfo_screenwidth() // 4
                 width = window.winfo_screenwidth() - sidelength
                 height = window.winfo_screenheight() - sidelength
-                window.geometry(f"{sidelength}x{sidelength}+{random.randint(0, width)}+{random.randint(0, height)}") 
                 # 设置窗口位置和大小
+                window.geometry(f"{sidelength}x{sidelength}+{random.randint(0, width)}+{random.randint(0, height)}") 
+                
             def open_window(index):
                 window = tk.Toplevel(root)
                 set_window2(window)
-                window.after(200, lambda: open_window(index + 1))
+                window.after(200, lambda: open_window(index + 1))  # 200ms后执行匿名函数, 匿名函数调用 open_window 函数, 传参 index+1
                 window.after(1000, lambda: window.destroy())
+
             open_window(0)
-            
+
         # 10.测试
         if self.contract_list[9][2] == "√":
             def test():
@@ -302,6 +299,7 @@ class Contract:
             testthread.start()
         root.mainloop()
 
+        #===================================================================================#
 
 
 # 主类 
@@ -311,22 +309,22 @@ class MainProgram:
 
     def restart_program(self, e):
         print("Restarting program...")
-        screen = rotatescreen.get_primary_display() # 获取主显示器对象
-        screen.rotate_to(0)
-        del self.contract
-        keyboard.press_and_release('backspace')
+        screen = rotatescreen.get_primary_display()  # 获取主显示器对象
+        screen.rotate_to(0)  # 屏幕旋转归零
+        del self.contract  # 删除实体释放内存
+        keyboard.press_and_release('backspace') # 删掉输入框里新输入的 r
         python = sys.executable
-        os.execl(python, python, *sys.argv)
+        os.execl(python, python, *sys.argv)  # 启动一个新的 Python 进程
 
     def run(self):
         keyboard.on_press_key('r', self.restart_program)
         while True:
             print("请选择挑战合约: \n")
-            os.system('cls')
-            self.contract.show_info()    # 展示合约列表
-            if self.contract.choose() == 1:    # 选择合约条目
+            os.system('cls')  # 清除终端内的文字
+            self.contract.show_info()  # 展示合约列表
+            if self.contract.choose() == 1:  # 选择合约条目
                 break
-        self.contract.run()
+        self.contract.run()  # 开始运行合约
         keyboard.wait()
 
 # 主函数入口点
